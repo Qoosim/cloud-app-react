f42744cba83a3adea6d64d819fab86cd501e9bc9
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-04-14 18:30:08 +0100

    Add functionalities for all the navigation buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { Link, animateScroll as scroll } from 'react-scroll'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -7,17 +8,20 @@ const Navbar = () => {
   const menuClick = () => {
     setNav(!nav);
   }
+
+  const handleClose = () => setNav(!nav);
+
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
           <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND.</h1>
           <ul className="hidden md:flex">
-            <li>Home</li>
-            <li>About</li>
-            <li>Support</li>
-            <li>Platform</li>
-            <li>Pricing</li>
+            <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
+            <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+            <li><Link to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
+            <li><Link to="platform" smooth={true} offset={-100} duration={500}>Platform</Link></li>
+            <li><Link to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
           </ul>
         </div>
         <div className="hidden md:flex pr-4">
@@ -30,11 +34,21 @@ const Navbar = () => {
       </div>
 
       <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
-        <li className="border-b-2 border-zinc-300 w-full">Home</li>
-        <li className="border-b-2 border-zinc-300 w-full">About</li>
-        <li className="border-b-2 border-zinc-300 w-full">Support</li>
-        <li className="border-b-2 border-zinc-300 w-full">Platform</li>
-        <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link to="home" smooth={true} duration={500} onClick={handleClose}>Home</Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link to="about" smooth={true} offset={-200} duration={500} onClick={handleClose}>About</Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link to="support" smooth={true} offset={-50} duration={500} onClick={handleClose}>Support</Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link to="platform" smooth={true} offset={-100} duration={500} onClick={handleClose}>Platform</Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link to="pricing" smooth={true} offset={-50} duration={500} onClick={handleClose}>Pricing</Link>
+        </li>
         <div className="flex flex-col my-4">
           <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">Sign In</button>
           <button className="px-8 py-3">Sign Up</button>
