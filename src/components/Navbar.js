14c8b2b478a6963f6daac7a7babe423fa4a2f610
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-04-15 11:21:09 +0100

    Add cursor pointer property to the list items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,8 +17,8 @@ const Navbar = () => {
         <div className="flex items-center">
           <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND.</h1>
           <ul className="hidden md:flex">
-            <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-            <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+            <li className="cursor-pointer"><Link to="home" smooth={true} duration={500}>Home</Link></li>
+            <li ><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
             <li><Link to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
             <li><Link to="platform" smooth={true} offset={-100} duration={500}>Platform</Link></li>
             <li><Link to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
